52fe34d8889d837a75136308695b3bf0803fd430
vasilii <porokh.vs@gmail.com>
2023-11-27 13:35:24 +0300

    process

--- src/studio.ts
+++ src/studio.ts
@@ -12,17 +12,15 @@ export class Studio {
         this.canvas = document.getElementById('renderCanvas') as HTMLCanvasElement;
         this.engine = new BABYLON.Engine(this.canvas, true, { preserveDrawingBuffer: true, stencil: true })
         this.scene = new BABYLON.Scene(this.engine)
-
         this.camera = new BABYLON.ArcRotateCamera("camera1",  0, 0, 0, new BABYLON.Vector3(0, 0, 0), this.scene)
         this.camera.setPosition(new BABYLON.Vector3(0, 5, -30))
         this.camera.attachControl(this.canvas, true)
 
         const light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), this.scene)
 
-        this.engine.runRenderLoop(() =>{
+        this.engine.runRenderLoop(() => {
             this.scene.render()
         })
-
         window.addEventListener('resize', () => {
             this.engine.resize()
         })
